1c66b60fe6f8312542100de343f3e2283a40f445
DiegoG2212 <42857014+DiegoG2212@users.noreply.github.com>
2018-10-11 17:00:17 -0700

    test main

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 
+publicCertificate = "/Users/mcastro/Documents/public.pem"
+privateCertificate = "/Users/mcastro/Documents/private.pem"
+import os
+import base64
+
+iv = os.urandom(16)  # generate iv
+byteMSG = bytes("CECS478", "UTF-8")
+test = iv + byteMSG
+
+print(iv)
+print(type(byteMSG))
+print(byteMSG)
+print(test)
+
+iv2 = test[:16]
+print("iv2:", iv2)
