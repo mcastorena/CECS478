af88ec7e0be80c424250cf9289a9cde030e4cd54
mcastorena <miguel.angel.castorena@gmail.com>
2018-12-06 15:57:40 -0800

    added WIP client files

---
+++ client.py
@@ -0,0 +1,52 @@
+from encryptor import Encryptor
+from decryptor import Decryptor
+import requests
+publicCertificate = "/Users/mcastro/Desktop/public.pem"
+privateCertificate = "/Users/mcastro/Desktop/private.pem"
+
+class Client(object):
+    def __init__(self, url):
+        self.URL = url
+        self.payload = None
+        self.email = None
+        self.password = None
+        self.response = None
+        self.JWT = None
+        self.token = None
+
+    def login(self, email, password):
+        try:
+            self.payload = {'email': email, 'password': password}
+            self.response = requests.post(self.URL, data=self.payload)          #store server response
+            self.JWT = self.response.json()                                     #get payload
+            self.token = self.JWT['token']                                      #get token
+            print("Login succesful")
+        except:
+            raise ValueError('Login failed')
+
+    def postMsg(self, URL, recipient, message):
+        print("Sending message...")
+        head = {'Authorization': self.token}
+        body = {'to': recipient, 'message_body': message}
+
+        msgResponse = requests.post(URL, headers=head, data=body)
+        # try:
+        #     msgResponse = requests.post(URL, headers= head, data= body)
+        # except:
+        #     raise ValueError('Message could not be sent')
+
+
+
+    def getMsg(self, URL):
+        print("Fetching new messages...")
+        head = {'Authorization': self.token}
+        msgResponse = requests.get(URL, headers=head)
+        return msgResponse
+        # try:
+        #     msgResponse = requests.get(URL, headers=head)
+        #     return msgResponse
+        # except:
+        #     raise ValueError('Messages could not be fetched')
+
+
+
---
+++ clientMain.py
@@ -0,0 +1,59 @@
+from encryptor import Encryptor
+from decryptor import Decryptor
+from client import Client
+import requests
+publicCertificate = "/Users/mcastro/Desktop/public.pem"
+privateCertificate = "/Users/mcastro/Desktop/private.pem"
+jsonFile = "/Users/mcastro/Desktop/encrypt.json"
+loginURL = "http://54.153.48.132:3000/api/authenticate"
+chatURL = "http://54.153.48.132:3000/api/chat"
+
+keysize = 32        #32 bytes
+blockSize = 128     #128 bits
+ivSize = 16         #16 bytes
+
+inputEmail = input("Enter email address: ")
+inputPassword = input("Enter password: ")
+
+print("Logging in user: ", inputEmail)
+myClient = Client(loginURL)
+try:
+    myClient.login(inputEmail, inputPassword)               #login with given email and password
+except ValueError:                                          #if login fails prompt user for username and password again
+    inputEmail = input("Enter email address: ")
+    inputPassword = input("Enter password: ")
+    myClient.login(inputEmail, inputPassword)
+
+session = True                                              #session boolean set to true
+myEncryptor = Encryptor(keysize, blockSize, ivSize, publicCertificate, jsonFile)
+myDecryptor = Decryptor(keysize, blockSize, ivSize, jsonFile, privateCertificate)
+
+
+while(session):                                             #prompt user to send/check messages
+    print("Input options: \n\t1: Send message \n\t2: Check messages \n\t3: Logout\n")
+    option = input("Input choice: ")
+
+    if(option == '1'):                                        #send message
+        recipient = input("Enter recipient email: ")
+        msg = input("Enter message:\t")
+        encryptedMsg = myEncryptor.encrypt(msg)             #encrypt message before sending
+
+        myClient.postMsg(chatURL, recipient, encryptedMsg)
+
+        # try:
+        #     myClient.postMsg(chatURL, encryptedMsg)
+        # except:
+        #     print("Message could not be sent")
+    if(option == '2'):                                        #check messages
+        msgResponse = myClient.getMsg(chatURL)
+        print(msgResponse.json())
+        # try:
+        #     print(myClient.getMsg(chatURL))
+        # except:
+        #     print("Error fetching messages")
+    if(option == '3'):
+        session = False                                     #session boolean = false
+        print("Logging out user: ", inputEmail)
+        exit(0)                                             #exit program
+    else:                                                   #invalid input
+        print("Invalid option selected\n")
