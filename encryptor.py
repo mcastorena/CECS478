8d9549cd33b6014c108bba56c41ec32377e49659
mcastorena <miguel.angel.castorena@gmail.com>
2018-12-06 15:59:13 -0800

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -10,10 +10,10 @@ from cryptography.hazmat.primitives.asymmetric import padding
 
 
 class Encryptor(object):
-    def __init__(self, keySize, blockSize, ivSize, message, publicCertificate, jsonFile):
+    def __init__(self, keySize, blockSize, ivSize, publicCertificate, jsonFile):
         self.keySize = keySize
         self.publicCertificate = publicCertificate
-        self.message = message
+        self.message = None
         self.ivSize = ivSize
         self.blockSize = blockSize
         self.AESKey = None
@@ -22,7 +22,8 @@ class Encryptor(object):
         self.jsonFile = jsonFile
 
 
-    def encrypt(self):
+    def encrypt(self, message):
+        self.message = message
         b64MSG =  base64.encodebytes(self.AESEncrypt())            #AES encrypt message then bytes to b64
         ASCIIMsg = b64MSG.decode('ascii')                           #b64 to ascii
         b64HMAC = base64.encodebytes(self.HMAC())                    #HMAC then digest to b64
@@ -30,16 +31,17 @@ class Encryptor(object):
         b64Keys = base64.encodebytes(self.RSAEncrypt())             #RSA encrypt keys then bytes to b64
         ASCIIKeys = b64Keys.decode('ascii')                         #b64 to ascii
         data = {'Msg': ASCIIMsg, 'HMAC': ASCIIHMAC, 'Keys': ASCIIKeys}  #create dictionary with our data
-        jsonData = json.dumps(data)                                                #dictionary to json
-        with open(self.jsonFile, 'w') as outFile:
-            json.dump(jsonData, outFile)                                            #write to jsonFile
+        return data
+        #jsonData = json.dumps(data)                                                #dictionary to json object
+        #with open(self.jsonFile, 'w') as outFile:
+        #    json.dump(jsonData, outFile)                                            #write to jsonFile
 
     def AESEncrypt(self):
         key = os.urandom(self.keySize)  # generate key
         self.AESKey = key               # save AESKey
         iv = os.urandom(self.ivSize)  # generate iv
         cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())  # create cipher object using AES in CBC mode using the generated key and iv
-        AESEncryptor = cipher.encryptor()  # create encryptor object
+        AESEncryptor = cipher.encryptor()                                       # create encryptor object from cipher object
         byteMSG = bytes(self.message, "UTF-8")                                  #message string to bytes
         bytesIVMSG = iv + byteMSG                                               #prepend IV to MSG
         padder = p.PKCS7(self.blockSize).padder()  # create padder object using PKCS7, blocksize = 128
@@ -53,13 +55,13 @@ class Encryptor(object):
         self.HMACKey = HMACKey              # save HMAC key
         h = hmac.HMAC(HMACKey, hashes.SHA256(), backend=default_backend())  # create hash algorithm object
         h.update(self.cipherText)  # bytes to hash and authenticate
-        digest = h.finalize()  # finalize hash and return digest as bytes
-        return digest
+        return h.finalize()  # finalize hash and return digest as bytes
+
 
     def RSAEncrypt(self):
         with open(self.publicCertificate, "rb") as publicKey:                                #load public key from filepath
             public_key = serialization.load_pem_public_key(publicKey.read(),backend=default_backend())
-        keys = self.AESKey + self.HMACKey
+        keys = self.AESKey + self.HMACKey                                                    #concatenate keys
         cipherKeys = public_key.encrypt(keys, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(),label=None))  # encrypt keys
         return cipherKeys
 
