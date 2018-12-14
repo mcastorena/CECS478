de9731546252718a13785cd366f4f4de750707b9
mcastorena <miguel.angel.castorena@gmail.com>
2018-12-13 21:54:11 -0800

    Add files via upload

---
+++ chatAPI/app/models/chat.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Schema defines how chat messages will be stored in MongoDB
+const ChatSchema = new mongoose.Schema({
+  from: {
+    type: String,
+    required: true
+  },
+  to: {
+    type: String,
+    required: true
+  },
+  message_body: {
+    type: String,
+    required: true
+  },
+  msgRead: {
+    type: Boolean,
+    default: false
+  }
+},
+{
+  timestamps: true // Saves createdAt and updatedAt as dates. createdAt will be our timestamp.
+}
+);
+
+module.exports = mongoose.model('Chat', ChatSchema);
